3d9e2ccd9f39201ed277bd97eaa5d36e487f494b
sagar117 <sagarpawar3007@gmail.com>
2024-09-24 17:17:45 -0400

    routing 6

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose'); // For MongoDB connection
 require('dotenv').config(); // Load environment variables
 
 // const config = require('./config/config'); // Configuration file
-const { router: authRoutes, authenticateJWT } = require('./routes/auth'); // Importing auth routes
+// const { router: authRoutes, authenticateJWT } = require('./routes/auth'); // Importing auth routes
 // const orgRoutes = require('./routes/org'); // Import organization routes
 
 const app = express();
