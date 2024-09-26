208030ac986c5d26e2c6a808ef5cf004e21e8bc3
sagar117 <sagarpawar3007@gmail.com>
2024-09-26 14:25:34 -0400

    mongodb 4

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -74,3 +74,25 @@ exports.createOrganization = async (req, res) => {
         res.status(500).json({ message: 'Error creating organization', error });
     }
 };
+
+// Get a user by username
+const getUserByUsername = async (req, res) => {
+    const { username } = req.params;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        res.status(500).json({ message: 'Error fetching user', error });
+    }
+};
+
+// Exporting the functions
+module.exports = {
+    // ... other exports,
+    getUserByUsername,
+};
