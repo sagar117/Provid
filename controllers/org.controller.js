7783d31ceaef4a4e609921a850d330236858642d
sagar117 <sagarpawar3007@gmail.com>
2024-09-28 23:26:25 -0400

    logging 15

--- controllers/org.controller.js
+++ controllers/org.controller.js
@@ -1,5 +1,5 @@
-const Org = require('../models/Organization'); // Import the Organization model
-const User = require('../models/User'); // Import the User model
+const Org = require('../models/organization.model'); // Import the Organization model
+const User = require('../models/user.model'); // Import the User model
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
