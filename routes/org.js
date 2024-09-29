59f7a577482545d4411e8f6656567fb2f0b0d38b
sagar117 <sagarpawar3007@gmail.com>
2024-09-29 01:05:34 -0400

    guides 3

--- routes/org.js
+++ routes/org.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const orgController = require('../controllers/org.controller');
+const authMiddleware = require('../middleware/auth');
 
 // Route to create an organization and user simultaneously
 router.post('/create', orgController.createOrgAndUser);
-router.post('/saveRecording', authMiddleware.verifyToken, orgController.saveRecording);
+router.post('/saveRecording', authMiddleware.authenticateJWT, orgController.saveRecording);
 
 
 module.exports = router;
