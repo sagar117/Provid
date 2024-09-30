4ec8c3e885731ef1b69f65574fa767e65a275986
sagar117 <sagarpawar3007@gmail.com>
2024-09-30 08:06:45 -0400

    me API 7

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -130,6 +130,8 @@ const me = async (req, res) => {
             return res.status(404).json({ message: 'User not found' });
         }
 
+        console.log(user.org_id);
+
         // Fetch organization details using the user's orgId
         const organization = await Organization.findById(user.orgId);
         if (!organization) {
