89ae5c3168f42dda0448a972a88d687b0d2f2eae
sagar117 <sagarpawar3007@gmail.com>
2024-10-02 07:41:34 -0400

    Login API response 4

--- models/Guides.js
+++ models/Guides.js
@@ -4,7 +4,7 @@ const guideSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     events: { type: [Object], required: true }, // Array of event objects
-    organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization'},
+    organization: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
 });
 
